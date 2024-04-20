d59bc418a0f5965d763f87b648cb4213627c76a3
Luciano Sampaio <lsampaioweb@gmail.com>
2024-04-20 13:30:22 -0300

    chore: Remove Terraform files because this is a Packer project.

--- terraform/output.tf
+++
@@ -1,8 +0,0 @@
-output "virtual_machines" {
-  value = module.homelab_project.vms
-}
-
-output "app_url" {
-  description = "URL of the application"
-  value       = ["https://XXX.homelab", "https://XXX-01.homelab", "https://XXX-02.homelab"]
-}
